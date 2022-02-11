f9b42e0248c62fb77c6183010a3414e74a5b497b
Antonio Jimenez Adalia <ajimenezadalia@gmail.com>
2022-02-11 23:26:53 +0100

    more changes

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 
 
-from ayto_madrid.src.main_classes import TransportData, StationInfo, GeodesicDistance
-from ayto_madrid.src.sub_classes import Emt, EmtStation, BiciMad, BiciMadStation
-from ayto_madrid.src.utils import get_station_info, get_distance, read_config_file
+from src.main_classes import TransportData, StationInfo, GeodesicDistance
+from src.sub_classes import Emt, EmtStation, BiciMad, BiciMadStation
+from src.utils import get_station_info, get_distance, read_config_file
